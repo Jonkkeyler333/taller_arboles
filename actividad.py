fcb670941161dfd9bde61d353967ac7ef527479d
Jonkkeyler333 <82979079+Jonkkeyler333@users.noreply.github.com>
2023-06-15 07:31:43 -0500

    creando la clase arbol

--- actividad.py
+++ actividad.py
@@ -1,7 +1,18 @@
-class Arbol:
-  class nodo:
-    def __init__(self,element:any,parent) -> None:
-      self.element=element
-      self.parent=parent
-      
+class Nodo:
+  def __init__(self,element:any,parent=None) -> None:
+    self.element=element
+    self.parent=parent
     
+class Arbol:
+  def __init__(self,root:Nodo) -> None:
+    self.root=root
+    self.peso=0
+    self.altura=0
+    self.orden=0
+  
+  def add(self,element:any,parent=None) -> None:
+    new_nodo=Nodo(element,parent)
+    if self.peso==0:
+      self.root=new_nodo
+    else:
+      pass
